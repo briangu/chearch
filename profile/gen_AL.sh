6636bd52949117097e1517b5657b6fa3b13c3bcb
Brian Guarraci <bguarraci@twitter.com>
2015-06-14 06:34:39 -0700

    more data

--- profile/gen_AL.sh
+++ profile/gen_AL.sh
@@ -1,5 +1,5 @@
 grep "^AL" $1 | cut -f 3 -d ',' | python ../find_min.py $1 >> ../out/AL.csv
-grep "^B" $1 | cut -f 3 -d ',' | python ../find_min.py $1 >> ../out/B.csv
-grep "^C" $1 | cut -f 3 -d ',' | python ../find_min.py $1 >> ../out/C.csv
-grep "^D" $1 | cut -f 3 -d ',' | python ../find_min.py $1 >> ../out/D.csv
-grep "^E" $1 | cut -f 3 -d ',' | python ../find_min.py $1 >> ../out/E.csv
+grep "^B,2" $1 | cut -f 3 -d ',' | python ../find_min.py $1 >> ../out/B.csv
+grep "^C,4" $1 | cut -f 3 -d ',' | python ../find_min.py $1 >> ../out/C.csv
+grep "^D,2" $1 | cut -f 3 -d ',' | python ../find_min.py $1 >> ../out/D.csv
+grep "^E,4" $1 | cut -f 3 -d ',' | python ../find_min.py $1 >> ../out/E.csv
